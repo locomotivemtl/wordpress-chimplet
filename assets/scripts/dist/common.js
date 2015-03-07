4e6d8b472b2bbc1aadf6349b119f394aebf188ae
Chauncey McAskill <shimoroka@gmail.com>
2015-03-06 20:52:34 -0500

    Added event to handle conditional display of elements.

--- assets/scripts/dist/common.js
+++ assets/scripts/dist/common.js
@@ -15,6 +15,45 @@ String.prototype.escapeSelector = function ( find ) {
 /* global jQuery */
 
 /**
+ * Conditional Display
+ * ==========================================================================
+ * @group  Chimplet
+ * @author Locomotive
+ */
+
+(function ($) {
+	'use strict';
+
+	var Condition = {
+		toggle: function (event)
+		{
+			console.group( 'Condition' );
+
+			var $trigger, $targets, $shown, $hidden, condition, value;
+
+			$trigger = $( event.currentTarget );
+
+			condition = 'data-condition-' + $trigger.data( 'condition-key' );
+
+			value = $trigger.val();
+
+			$targets = $( '[' + condition + ']' );
+
+			$shown  = $targets.filter('[' + condition + '="' + value + '"]').removeClass( 'hidden' );
+
+			$hidden = $targets.not('[' + condition + '="' + value + '"]').addClass( 'hidden' );
+
+			console.groupEnd();
+		}
+	};
+
+	$('.chimplet-wrap').on( 'change.chimplet.condition', '[data-condition-key]', Condition.toggle );
+
+}(jQuery));
+
+/* global jQuery */
+
+/**
  * Toggle Checkboxes
  * ==========================================================================
  * @group  Chimplet
