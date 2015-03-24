0922d97af53e0dcf25936761264f606dc7c4e472
Chauncey McAskill <chauncey@locomotive.ca>
2015-03-24 15:34:15 -0400

    Scripts: Improved toggling of checkboxes with a scope option.

--- assets/scripts/src/chimplet/toggle-checkboxes.js
+++ assets/scripts/src/chimplet/toggle-checkboxes.js
@@ -13,27 +13,34 @@
 	var Checkbox = {
 		toggle: function (event)
 		{
-			var $trigger, $checkboxes, $master;
+			var $trigger, $checked, $checkboxes, $master, $scope;
 
 			$trigger = $( event.currentTarget );
+
+			$scope = $trigger.parents('[data-checkbox-scoped]');
+
+			$checkboxes = $( ':checkbox[name="' + $trigger.attr('name').escapeSelector() + '"]', ( $scope.length ? $scope : null ) ).not(':disabled');
 
 			if ( 'all' === $trigger.val() ) {
 				$master = $trigger;
 
-				$checkboxes = $(':checkbox[name="' + $trigger.attr('name').escapeSelector() + '"]').not(':disabled').not( $trigger );
+				$checkboxes = $checkboxes.not( $master );
 
 				$checkboxes.prop( 'checked', $master.prop('checked') );
 			}
 			else {
-				$checkboxes = $(':checkbox[name="' + $trigger.attr('name').escapeSelector() + '"]').not(':disabled');
-
 				$master = $checkboxes.filter('[value="all"]');
+				$checkboxes = $checkboxes.not( $master );
+
+				$checked = $checkboxes.filter(':checked');
 
 				if ( $master.length ) {
 
 					$checkboxes = $checkboxes.not( $master );
 
-					$master.prop( 'checked', ( $checkboxes.length === $checkboxes.filter(':checked').length ) );
+					$master
+						.prop( 'checked', $checked.length > 0 )
+						.prop( 'indeterminate', ( $checked.length > 0 && $checked.length < $checkboxes.length ) );
 				}
 			}
 		}
